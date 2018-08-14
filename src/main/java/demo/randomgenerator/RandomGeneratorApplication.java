8aa315b08116a64204d3bb10b6de2f89cf985917
Roland Huß <roland@ro14nd.de>
2018-08-14 12:30:16 +0200

    Added RandomGeneratorService

--- src/main/java/demo/randomgenerator/RandomGeneratorApplication.java
+++ src/main/java/demo/randomgenerator/RandomGeneratorApplication.java
@@ -1,12 +1,31 @@
 package demo.randomgenerator;
 
+import java.util.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RandomGeneratorApplication {
+
+	// Unique UUID to identify service
+	private static UUID id = UUID.randomUUID();
+
+	// Random number generator
+	private static Random random = new Random();
 
 	public static void main(String[] args) {
 		SpringApplication.run(RandomGeneratorApplication.class, args);
+	}
+
+	@RequestMapping(value = "/", produces = "application/json")
+	public Map getRandom() {
+		Map ret = new HashMap();
+		ret.put("id", id);
+		ret.put("random", random.nextInt());
+		return ret;
 	}
 }
